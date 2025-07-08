9ed7aa56088096957a8e36779e6f14dd916ecd45
idknevo <nevoexists@gmail.com>
2025-07-08 23:25:12 +0300

    update & powershell

---
+++ powershell/powershell.ps1
@@ -0,0 +1,46 @@
+oh-my-posh init pwsh --config 'E:\visual-studio\oh-my-posh\nevo.omp.json' | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+Set-Alias vscode "C:\Users\nevoo\AppData\Local\Programs\Microsoft VS Code\bin\code.cmd"
+
+Import-Module PSReadLine
+Set-PSReadLineKeyHandler -Key Tab -Function AcceptSuggestion
+Remove-Item alias:cd -Force
+
+function cd {
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string]$args
+    )
+
+    if ($args -eq "") { Set-Location } else { Set-Location $args }
+
+    $curr_dir = GetCurrentDir(Get-Location)
+    $title = $curr_dir
+    wezterm cli set-tab-title $title;
+}
+
+function GetCurrentDir {
+    param (
+      [string]$path = ""
+    )
+    if ($path -eq "") {
+      $path = Get-Location
+    }
+
+    if ($path -eq "$env:USERPROFILE") {
+      return "~"
+    }
+
+    return Split-Path ($path) -Leaf
+}
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,137 +1,3 @@
--- local wezterm = require("wezterm")
--- local act = wezterm.action
--- local config = wezterm.config_builder()
-
--- config.initial_rows = 24
--- config.max_fps = 144
--- config.default_cursor_style = "BlinkingBar"
--- config.animation_fps = 1
--- config.cursor_blink_rate = 500
--- config.term = "xterm-256color"
--- config.font = wezterm.font("FiraCode Nerd Font")
--- config.cell_width = 0.9
-
--- config.window_background_opacity = 1
--- config.prefer_egl = false
--- config.front_end = "OpenGL"
--- config.font_size = 14
-
--- config.window_padding = {
--- 	left = 0,
--- 	right = 0,
--- 	top = 0,
--- 	bottom = 0,
--- }
-
--- config.use_fancy_tab_bar = false
--- config.tab_bar_at_bottom = true
--- config.window_close_confirmation = "NeverPrompt"
-
--- -- start in a certain size
--- wezterm.on("gui-startup", function(cmd)
---   local screen = wezterm.gui.screens().active
---   local ratio = 0.7
---   local width, height = screen.width * ratio, screen.height * ratio
---   local tab, pane, window = wezterm.mux.spawn_window {
---     position = {
---       x = (screen.width - width) / 2,
---       y = (screen.height - height) / 2,
---       origin = 'ActiveScreen' }
---   }
---   if window then
---   window:gui_window():set_inner_size(width, height)
---   end
--- end)
-
--- -- start in full screen mode
--- -- local mux = wezterm.mux
--- -- wezterm.on('gui-startup', function()
--- --   local tab, pane, window = mux.spawn_window({})
--- --   window:gui_window():toggle_fullscreen()
--- -- end)
-
--- config.color_scheme = "Cloud (terminal.sexy)"
--- config.colors = {
--- 	background = "#0c0b0f",
--- 	cursor_border = "#ffffff",
--- 	cursor_bg = "#ffffff",
-
--- 	tab_bar = {
--- 		background = "#0c0b0f",
--- 		active_tab = {
--- 			bg_color = "#0c0b0f",
--- 			fg_color = "#fff",
--- 			intensity = "Normal",
--- 			underline = "None",
--- 			italic = false,
--- 			strikethrough = false,
--- 		},
--- 		inactive_tab = {
--- 			bg_color = "#0c0b0f",
--- 			fg_color = "#f8f2f5",
--- 			intensity = "Normal",
--- 			underline = "None",
--- 			italic = false,
--- 			strikethrough = false,
--- 		},
-
--- 		new_tab = {
--- 			bg_color = "#0c0b0f",
--- 			fg_color = "white",
--- 		},
--- 	},
--- }
-
--- config.window_frame = {
--- 	font = wezterm.font({ family = "mononoki", weight = "Regular" }),
--- 	active_titlebar_bg = "#0c0b0f",
--- }
-
--- config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
--- config.default_prog = { "pwsh.exe", "-NoLogo"}
--- config.initial_cols = 80
-
--- return config
-
--- WezTerm Keybindings Documentation by dragonlobster
--- ===================================================
--- Leader Key:
--- The leader key is set to ALT + q, with a timeout of 2000 milliseconds (2 seconds).
--- To execute any keybinding, press the leader key (ALT + q) first, then the corresponding key.
-
--- Keybindings:
--- 1. Tab Management:
---    - LEADER + c: Create a new tab in the current pane's domain.
---    - LEADER + x: Close the current pane (with confirmation).
---    - LEADER + b: Switch to the previous tab.
---    - LEADER + n: Switch to the next tab.
---    - LEADER + <number>: Switch to a specific tab (0–9).
-
--- 2. Pane Splitting:
---    - LEADER + |: Split the current pane horizontally into two panes.
---    - LEADER + -: Split the current pane vertically into two panes.
-
--- 3. Pane Navigation:
---    - LEADER + h: Move to the pane on the left.
---    - LEADER + j: Move to the pane below.
---    - LEADER + k: Move to the pane above.
---    - LEADER + l: Move to the pane on the right.
-
--- 4. Pane Resizing:
---    - LEADER + LeftArrow: Increase the pane size to the left by 5 units.
---    - LEADER + RightArrow: Increase the pane size to the right by 5 units.
---    - LEADER + DownArrow: Increase the pane size downward by 5 units.
---    - LEADER + UpArrow: Increase the pane size upward by 5 units.
-
--- 5. Status Line:
---    - The status line indicates when the leader key is active, displaying an ocean wave emoji (🌊).
-
--- Miscellaneous Configurations:
--- - Tabs are shown even if there's only one tab.
--- - The tab bar is located at the bottom of the terminal window.
--- - Tab and split indices are zero-based.
-
-
 -- Pull in the wezterm API
 local wezterm = require "wezterm"
 
@@ -151,8 +17,6 @@ config.window_padding = {
 	bottom = 0,
 }
 config.default_cursor_style = "BlinkingBar"
-
--- -- start in a certain size
 wezterm.on("gui-startup", function(cmd)
   local screen = wezterm.gui.screens().active
   local ratio = 0.8
@@ -168,19 +32,8 @@ wezterm.on("gui-startup", function(cmd)
   end
 end)
 
-
--- -- start in full screen mode
--- local mux = wezterm.mux
--- wezterm.on('gui-startup', function()
---   local tab, pane, window = mux.spawn_window({})
---   window:gui_window():toggle_fullscreen()
--- end)
-
 config.default_prog = { "pwsh.exe", "-NoLogo"}
 
-
--- For example, changing the color scheme:
--- config.color_scheme = "Catppuccin Macchiato"
 config.font =
     wezterm.font("FiraCode Nerd Font")
 config.font_size = 16
@@ -302,7 +155,7 @@ wezterm.on("update-right-status", function(window, _)
     })
 end)
 
--- config.color_scheme = "Cloud (terminal.sexy)"
+config.color_scheme = "Cloud (terminal.sexy)"
 config.colors = {
 	background = "#0c0b0f",
 	cursor_border = "#ffffff",
@@ -326,9 +179,10 @@ config.colors = {
 			italic = false,
 			strikethrough = false,
 		},
+
 		new_tab = {
 			bg_color = "#0c0b0f",
-			fg_color = "#0F4C5C",
+			fg_color = "white",
 		},
 	},
 }
@@ -345,5 +199,4 @@ config.window_background_opacity = 1
 config.prefer_egl = false
 config.front_end = "OpenGL"
 
--- and finally, return the configuration to wezterm
 return config
